408554ed71f5029c4e55286bb388d4e506697c85
yorman <yorman.arango@sofka.com.co>
2023-02-01 08:13:05 -0500

    wip: se agrega componente de main-page para crear lista de personajes

--- src/app/dbz/main-page/main-page.component.ts
+++ src/app/dbz/main-page/main-page.component.ts
@@ -1,14 +1,30 @@
 import { Component } from '@angular/core';
 
+interface Character {
+  name: string;
+  power: number;
+}
+
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.component.html'
 })
 export class MainPageComponent {
 
+  characters : Character[] = [];
+
+  newCharacter : Character = {
+    name: '',
+    power: 0
+  }
+
   addCharacter(){
-    console.log('Agregué algo!!!');
-    
+
+    this.characters.push(this.newCharacter)
+    this.newCharacter = {
+      name: '',
+      power: 0
+    }    
   }
 
 }
